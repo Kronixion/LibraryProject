2d83702ee5f1bd6e2ddae63ad7739314c536444f
Kronixion <38648440+Kronixion@users.noreply.github.com>
2020-04-25 13:55:51 +0300

    beggining of the project

---
+++ library/account/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class SignInForm(forms.Form):
+    username = forms.CharField(max_length=150, widget = forms.TextInput()
+    password = forms.CharField(widget = forms.PasswordInput())
